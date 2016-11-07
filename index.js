8379a3f6763f963b230352693755b77f845d5adc
SluvsP <salomekbg@gmail.com>
2016-11-07 09:19:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+let heroes = [
+  { firstName: 'Tony', lastName: 'Stark', heroName: 'Iron Man' },
+  { firstName: 'Steve', lastName: 'Rogers', heroName: 'Captain America' },
+  { firstName: 'Barry', lastName: 'Allen', heroName: 'The Flash' },
+];
+
+const addInitialsToHeroes = heroes => heroes.map(hero => Object.assign({}, hero, {
+  initials: hero.firstName.charAt(0) + hero.lastName.charAt(0),
+}));
+
+let heroesWithInitials = addInitialsToHeroes(heroes);
